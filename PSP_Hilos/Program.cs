69122fad1e47b66511575d33d4f1daa79209e4b1
ftrugon <fjtrujillo@iesrafaelalberti.es>
2024-12-10 21:12:57 +0100

    comit para probar cosas

--- PSP_Hilos/Program.cs
+++ PSP_Hilos/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+class ThreadTest
+{
+    static void Main()
+    {
+        Thread t = new Thread (new ThreadStart (Go));
+        Thread t2 = new Thread (new ThreadStart (Go));
+        Thread t3 = new Thread (new ThreadStart (Go));
+        t.Start(); // Run Go() on the new thread.
+        t2.Start(); // Run Go() on the new thread.
+        t3.Start(); // Run Go() on the new thread.
+        //Go(); // Simultaneously run Go() in the main thread.
+        
+    }
+    static void Go()
+    {
+        Console.WriteLine ("hello!");
+    }
+}
